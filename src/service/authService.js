442e81fd28b4e334b3d6dd576b102fab0e5dc7ba
slowteady <vbn0213@gmail.com>
2023-08-04 00:19:34 +0900

    feat: 로그인 요청 로직 구현

--- src/service/authService.js
+++ src/service/authService.js
@@ -35,3 +35,43 @@ export const signupService = async (body) => {
     };
   }
 };
+
+// 로그인 요쳥
+export const signinService = async (body) => {
+  try {
+    const response = await axios.post(
+      `${API.PRE_ONBOARDING_API_URL}${API.SIGN_IN}`,
+      body
+    );
+    const { status } = response;
+    const { access_token } = response.data;
+    let isSuccess = false;
+
+    // 로그인 성공
+    if (status === 200) {
+      isSuccess = true;
+    }
+
+    // 로컬스토리지에 토큰 저장
+    if (access_token) {
+      localStorage.setItem("JWT", access_token);
+    }
+
+    return { isSuccess };
+  } catch (err) {
+    console.error(err);
+    let msg = "예상치 못한 문제가 발생했습니다";
+
+    if (
+      err.response.data.statusCode === 401 &&
+      err.response.data.message === "Unauthorized"
+    ) {
+      msg = "비밀번호가 틀렸습니다";
+    }
+
+    return {
+      isSuccess: false,
+      msg,
+    };
+  }
+};
