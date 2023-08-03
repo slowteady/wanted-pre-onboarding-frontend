837b42357514fca45397e1074f708a00d80a910c
slowteady <vbn0213@gmail.com>
2023-08-04 00:52:15 +0900

    feat: 라우터에 토큰 검증 필요한 컴포넌트들 Auth hoc 적용

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./components/hoc/Auth";
 import Signin from "./components/page/Signin";
 import Signup from "./components/page/Signup";
 
@@ -11,8 +12,22 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/signin" />} replace />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/signin"
+        element={
+          <Auth isException={true}>
+            <Signin />
+          </Auth>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <Auth isException={true}>
+            <Signup />
+          </Auth>
+        }
+      />
     </Routes>
   );
 };
