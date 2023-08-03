9cb1c7022492d7e051754882c48e7cc3d1c13fb0
slowteady <vbn0213@gmail.com>
2023-08-03 18:24:14 +0900

    feat: 회원가입 페이지 추가 및 라우팅 연결

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/page/Main";
+import Signup from "./components/page/Signup";
 
 // ----------------------------------------------------------------------
 // 라우터 컴포넌트
@@ -10,6 +11,7 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
