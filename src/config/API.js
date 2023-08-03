52d4c310336187d6979a8149eb64b0a5fe82bd2c
slowteady <vbn0213@gmail.com>
2023-08-03 23:33:58 +0900

    feat: API 설정 파일 따로 분기

---
+++ src/config/API.js
@@ -0,0 +1,11 @@
+// ----------------------------------------------------------------------
+// API 정보
+// ----------------------------------------------------------------------
+
+const API = {
+  PRE_ONBOARDING_API_URL: "https://www.pre-onboarding-selection-task.shop",
+  SIGN_UP: "/auth/signup",
+  SIGN_IN: "/auth/signin",
+};
+
+export default API;
