2e2efd238841ce2caea474243f91dc740a7dc733
slowteady <vbn0213@gmail.com>
2023-08-05 00:33:19 +0900

    feat: todo 리스트 props 받아서 렌더링 시켜주는 로직 추가

--- src/components/page/todo/TodoList.jsx
+++ src/components/page/todo/TodoList.jsx
@@ -1,11 +1,12 @@
-import { Box, Typography } from "@mui/material";
+import { Box, List, Typography } from "@mui/material";
 import React from "react";
+import TodoItems from "./TodoItems";
 
 // ----------------------------------------------------------------------
 // Todo 리스트 컴포넌트
 // ----------------------------------------------------------------------
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <>
       <Typography
@@ -15,7 +16,21 @@ const TodoList = () => {
       >
         Todo
       </Typography>
-      <Box sx={{ flex: 1, overflowY: "auto", mb: 3 }}></Box>
+      <Box sx={{ flex: 1, overflowY: "auto", mb: 3 }}>
+        <List>
+          {todos.map((t) => {
+            const { id, isCompleted, todo } = t;
+            return (
+              <TodoItems
+                key={id}
+                id={id}
+                text={todo}
+                isCompleted={isCompleted}
+              />
+            );
+          })}
+        </List>
+      </Box>
     </>
   );
 };
