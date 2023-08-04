287a74649bc6f9fedb677f1cbb014734c1a2a883
slowteady <vbn0213@gmail.com>
2023-08-04 22:50:56 +0900

    feat: Todo 리스트 컴포넌트 추가

--- src/components/page/todo/TodoList.jsx
+++ src/components/page/todo/TodoList.jsx
@@ -1,7 +1,24 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
+// ----------------------------------------------------------------------
+// TODO 리스트 컴포넌트
+// ----------------------------------------------------------------------
+
 const TodoList = () => {
-  return <div>Todo</div>;
+  return (
+    <>
+      <Typography
+        component="h1"
+        variant="h5"
+        sx={{ mb: 3, textAlign: "center" }}
+      >
+        TODO
+      </Typography>
+      <Box sx={{ flex: 1, overflowY: "auto", mb: 3 }}>
+      </Box>
+    </>
+  );
 };
 
 export default TodoList;
