0d11a3a06eeb7399fa06f43274213884154ece8a
slowteady <vbn0213@gmail.com>
2023-08-05 00:32:14 +0900

    feat: Provider 걷어내고 children 컴포넌트들에 할당해줄 props 제어 로직 추가

--- src/components/page/todo/TodoIndex.jsx
+++ src/components/page/todo/TodoIndex.jsx
@@ -1,21 +1,43 @@
-import React from "react";
-import { TodoProvider } from "../../state/todo/Context";
+import React, { useEffect, useState } from "react";
+import { getTodoService } from "../../../service/todoService";
 import TodoCreate from "./TodoCreate";
 import TodoLayout from "./TodoLayout";
 import TodoList from "./TodoList";
 
 // ----------------------------------------------------------------------
-// TODO 인덱스 페이지 컴포넌트
+// Todo 인덱스 페이지 컴포넌트
 // ----------------------------------------------------------------------
 
 const TodoIndex = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodoData();
+  }, []);
+
+  // Todo 추가
+  const handleAdd = () => {
+    getTodoData();
+  };
+
+  // Todo 데이터 요청
+  const getTodoData = async () => {
+    const response = await getTodoService();
+    const { isSuccess, msg, data } = response;
+
+    if (isSuccess) {
+      setTodos(data);
+    } else {
+      alert(msg);
+      return;
+    }
+  };
+
   return (
-    <TodoProvider>
-      <TodoLayout>
-        <TodoList />
-        <TodoCreate />
-      </TodoLayout>
-    </TodoProvider>
+    <TodoLayout>
+      <TodoList todos={todos} />
+      <TodoCreate onAdd={handleAdd} />
+    </TodoLayout>
   );
 };
 
