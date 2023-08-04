d8c660f572f1bccba929a9d8f5843376614b9970
slowteady <vbn0213@gmail.com>
2023-08-04 22:52:24 +0900

    feat: Todo 추가를 위한 api url 추가

--- src/config/API.js
+++ src/config/API.js
@@ -6,6 +6,7 @@ const API = {
   PRE_ONBOARDING_API_URL: "https://www.pre-onboarding-selection-task.shop",
   SIGN_UP: "/auth/signup",
   SIGN_IN: "/auth/signin",
+  ADD_TODOS: "/todos",
 };
 
 export default API;
